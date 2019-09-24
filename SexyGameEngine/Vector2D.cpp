3ebeba58094f152bcaefc2826abe61cf3d901a9c
Scott-Burnett <thebreakfastfactoryyay@gmail.com>
2019-09-24 10:23:31 +0200

    Vector2d Class

---
+++ SexyGameEngine/Vector2D.cpp
@@ -0,0 +1,78 @@
+#include "Vector2D.h"
+
+Vector2D::Vector2D() {
+	x = 0;
+	y = 0;
+}
+
+Vector2D::Vector2D(int x, int y) {
+	this->x = x;
+	this->y = y;
+}
+
+Vector2D::~Vector2D() {
+}
+
+Vector2D& Vector2D::Add(const Vector2D& v2) {
+	this->x += v2.x;
+	this->y += v2.y;
+
+	return *this;
+}
+
+Vector2D& Vector2D::Subtract(const Vector2D& v2) {
+	this->x -= v2.x;
+	this->y -= v2.y;
+
+	return *this;
+}
+
+Vector2D& Vector2D::Multiply(const Vector2D& v2) {
+	this->x *= v2.x;
+	this->y *= v2.y;
+
+	return *this;
+}
+
+Vector2D& Vector2D::Divide(const Vector2D& v2) {
+	this->x /= v2.x;
+	this->y /= v2.y;
+
+	return *this;
+}
+
+Vector2D& operator+(Vector2D& v1, const Vector2D& v2) {
+	return v1.Add(v2);
+}
+
+Vector2D& operator-(Vector2D& v1, const Vector2D& v2) {
+	return v1.Subtract(v2);
+}
+
+Vector2D& operator*(Vector2D& v1, const Vector2D& v2) {
+	return v1.Multiply(v2);
+}
+
+Vector2D& operator/(Vector2D& v1, const Vector2D& v2) {
+	return v1.Divide(v2);
+}
+
+Vector2D& Vector2D::operator+=(const Vector2D& v2) {
+	return this->Add(v2);
+}
+
+Vector2D& Vector2D::operator-=(const Vector2D& v2) {
+	return this->Subtract(v2);
+}
+
+Vector2D& Vector2D::operator*=(const Vector2D& v2) {
+	return this->Multiply(v2);
+}
+
+Vector2D& Vector2D::operator/=(const Vector2D& v2) {
+	return this->Divide(v2);
+}
+
+std::ostream& operator<< (std::ostream& stream, const Vector2D& vector) {
+	stream << "(" << vector.x << "," << vector.y << ")";
+}
---
+++ SexyGameEngine/Vector2D.h
@@ -0,0 +1,33 @@
+#pragma once
+/*  Class to represent 2D Point Vector 
+    Designed by: Let's Make Games
+	    profile:    https://www.youtube.com/user/creaper
+	    video:      https://www.youtube.com/watch?v=ESNN57ofa3w
+*/
+#include <iostream>
+
+class Vector2D {
+public:
+	int x, y;
+
+	Vector2D();
+	Vector2D(int x, int y);
+	~Vector2D();
+
+	Vector2D& Add(const Vector2D& v2);
+	Vector2D& Subtract(const Vector2D& v2);
+	Vector2D& Multiply(const Vector2D& v2);
+	Vector2D& Divide(const Vector2D& v2);
+
+	friend Vector2D& operator+(Vector2D& v1, const Vector2D& v2);
+	friend Vector2D& operator-(Vector2D& v1, const Vector2D& v2);
+	friend Vector2D& operator*(Vector2D& v1, const Vector2D& v2);
+	friend Vector2D& operator/(Vector2D& v1, const Vector2D& v2);
+
+	Vector2D& operator+=(const Vector2D& v2);
+	Vector2D& operator-=(const Vector2D& v2);
+	Vector2D& operator*=(const Vector2D& v2);
+	Vector2D& operator/=(const Vector2D& v2);
+
+	friend std::ostream& operator<< (std::ostream& stream, const Vector2D& vector);
+};
